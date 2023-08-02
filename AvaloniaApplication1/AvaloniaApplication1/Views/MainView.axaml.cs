16163886a2d3c034685efdf2d8c1d57a398b997b
Denis <dannaborisova@mail.ru>
2023-08-02 23:25:41 +0300

    Первая загркзка

---
+++ AvaloniaApplication1/AvaloniaApplication1/Views/MainView.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {
+        if (double.TryParse(celsius.Text, out double c))
+        {
+            var f = c*(9d / 5d) + 32;
+            fahrenheit.Text = f.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
+        Debug.WriteLine($"Click! Celsius = {celsius.Text}");
+    }
+}
---
+++ AvaloniaApplication1/AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
