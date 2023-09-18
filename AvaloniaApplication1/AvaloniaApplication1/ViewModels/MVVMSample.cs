4e04e0da3d84699a7b1cb2a1d61a832fe34b0540
Denis <dannaborisova@mail.ru>
2023-09-19 00:52:35 +0300

    удалил коменты в MVVM

--- AvaloniaApplication1/AvaloniaApplication1/ViewModels/MVVMSample.cs
+++ AvaloniaApplication1/AvaloniaApplication1/ViewModels/MVVMSample.cs
@@ -143,9 +143,6 @@ namespace AvaloniaApplication1.ViewModels
                     {
                         using (FileStream fs = new FileStream(files[0].Name, FileMode.OpenOrCreate))
                         {
-                            //    await using var stream = await files[0].OpenReadAsync();
-                            //using var streamReader = new StreamReader(stream);
-                            //var fileContent = await streamReader.ReadToEndAsync();
                             Employee? employeeFromXML = serializer.Deserialize(fs) as Employee;
 
                             if (employeeFromXML != null)
@@ -157,114 +154,6 @@ namespace AvaloniaApplication1.ViewModels
                 }
             }
         }
-
-        //public event PropertyChangedEventHandler? PropertyChanged;
-
-        //private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
-        //{
-        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        //}
-
-        //private string resultString;
-        //public string ResultString
-        //{
-        //    get
-        //    {
-        //        var first = string.Empty;
-        //        var second = string.Empty;
-        //        var middle = string.Empty;
-        //        var address = string.Empty;
-        //        if (LastName != null && LastName != string.Empty)
-        //            second = LastName;
-        //        if (FirstName != null && FirstName != string.Empty)
-        //            first = $" {FirstName.FirstOrDefault()}.";
-        //        if (MiddleName != null && MiddleName != string.Empty)
-        //            middle = $" {MiddleName.FirstOrDefault()}.";
-        //        if (Address != null && Address != string.Empty)
-        //            address = $" проживает по адресу: {Address}.";
-
-        //        var _result = $"{second}{first}{middle}{address}";
-
-        //        return _result;
-        //    }
-        //    private set
-        //    {
-        //        resultString = value; RaisePropertyChanged(nameof(ResultString));
-        //    }
-        //}
-
-        //private string? firstName;
-
-        //public string? FirstName
-        //{
-        //    get
-        //    {
-        //        return firstName;
-        //    }
-        //    set
-        //    {
-        //        if (firstName != value)
-        //        {
-        //            firstName = value; RaisePropertyChanged();
-        //            RaisePropertyChanged(nameof(ResultString));
-        //        }
-        //    }
-        //}
-        //private string? lastName;
-
-        //public string? LastName
-        //{
-        //    get
-        //    {
-        //        return lastName;
-        //    }
-        //    set
-        //    {
-        //        if (lastName != value)
-        //        {
-        //            lastName = value; RaisePropertyChanged();
-        //            RaisePropertyChanged(nameof(ResultString));
-        //        }
-        //    }
-        //}
-
-
-        //private string? middleName;
-
-        //public string? MiddleName
-        //{
-        //    get
-        //    {
-        //        return middleName;
-        //    }
-        //    set
-        //    {
-        //        if (middleName != value)
-        //        {
-        //            middleName = value; RaisePropertyChanged();
-        //            RaisePropertyChanged(nameof(ResultString));
-        //        }
-        //    }
-        //}
-
-
-        //private string? address;
-
-        //public string? Address
-        //{
-        //    get
-        //    {
-        //        return address;
-        //    }
-        //    set
-        //    {
-        //        if (address != value)
-        //        {
-        //            address = value; RaisePropertyChanged();
-        //            RaisePropertyChanged(nameof(ResultString));
-        //        }
-        //    }
-        //}
 
 
     }
